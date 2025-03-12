485d1847d623a50c4b7840d98e2c7b4153223ea1
Nutchanon-TR <nutchanon.charo@mail.kmutt.ac.th>
2025-03-13 02:54:45 +0700

    66115 [add] tailwind in HomePage Professor

--- src/components/ExamBlog.vue
+++ src/components/ExamBlog.vue
@@ -23,16 +23,30 @@ const byProfessor = (ownerId) => {
 </script>
 
 <template>
-  <div>
-    <h2>Component ExamManager</h2>
-    <div v-for="(exam, index) in examData" :key="index">
-      <hr>
-      <p>category: {{ exam.category }}</p>
-      <p>name: {{ exam.name }}</p>
-      <p>like: {{ exam.likes }}</p>
-      <p v-if="exam.papers.length > 1">question: {{ exam.papers[1].question }}</p>
-      <p>by professor: {{ byProfessor(exam.ownerExam_id) }}</p>
-      <hr>
-    </div>
-  </div>
-</template>
+   <div class="bg-black-100 min-h-screen">
+     <div class="p-6 mx-[50px]">
+       <div class="grid grid-cols-3 gap-12 mx-[50px] mt-[-20px]">
+         <button
+           v-for="(exam, index) in examData"
+           :key="index"
+           class="bg-blue-400 text-white rounded-xl shadow-md overflow-hidden w-full text-left flex flex-col hover:cursor-pointer"
+         >
+           <!-- Upper Section -->
+           <div class="p-4">
+             <h2 class="text-xl font-bold">{{ exam.category }}</h2>
+             <p class="font-semibold pb-[80px]">{{ exam.name }}</p>
+           </div>
+ 
+           <!-- Bottom Section (White Box) -->
+           <div class="flex justify-between items-center bg-white p-2 text-black border-t w-full">
+             <div class="flex items-center">
+               <span class="text-red-500 text-xl">❤️</span>
+               <span class="ml-1 font-semibold">{{ exam.likes }}</span>
+             </div>
+             <p class="text-gray-600 text-sm">BY {{ byProfessor(exam.ownerExam_id) }}</p>
+           </div>
+         </button>
+       </div>
+     </div>
+   </div>
+ </template>
--- src/views/Professor/HomePagePro.vue
+++ src/views/Professor/HomePagePro.vue
@@ -3,7 +3,15 @@ import {} from "vue";
 import ExamBlog from "@/components/ExamBlog.vue";
 </script>
 <template>
-  Home Professors
+<div class="p-6 mx-[100px]">
+       <div class="flex justify-between items-center">
+         <div>
+           <h1 class="text-2xl font-bold">PUBLISHED CLASSES</h1>
+           <p class="text-gray-500 mb-4">These classes are available to students.</p>
+         </div>
+         <h2 class="text-2xl font-bold text-black">STUDENT</h2>
+       </div>
+       </div>
   <div>
     <ExamBlog />
   </div>
