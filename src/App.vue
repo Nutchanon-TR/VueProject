8abebb19e050e498f97c956a37e435129783cae0
Dozen2 <thanakorn.phos@kmutt.ac.th>
2025-03-14 20:15:45 +0700

    Thanakorn-116[Add] Create The Exam(POST)version Beta(unfinish)

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ onMounted(async() => {
   console.log("userLoginData: ",userLoginData)
   console.log("userLoginData.email: ",userLoginData.email)
   console.log("userLoginData.firstName: ",userLoginData.name)
-  console.log("userLoginData.history: ",userLoginData.history[1])
+  console.log("userLoginData.history: ",userLoginData.history)
 })
 </script>
 
